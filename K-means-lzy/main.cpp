9ec87add23f9d7f7753bf45effca3f6f0ac3b9e4
cslzy <lizhenyang_2008@163.com>
2016-05-16 10:10:50 +0800

    Initial Commit

---
+++ K-means-lzy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  K-means-lzy
+//
+//  Created by cslzy on 16/5/16.
+//  Copyright © 2016年 CY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
